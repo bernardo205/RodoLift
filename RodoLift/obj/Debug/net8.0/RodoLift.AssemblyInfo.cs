6d9ac03bf766445f981b6459ec7eccafc98691a0
bernardo205 <bernardo.ribeiro.2004@sapo.pt>
2024-03-16 17:52:59 +0000

    some reactions

--- RodoLift/obj/Debug/net8.0/RodoLift.AssemblyInfo.cs
+++ RodoLift/obj/Debug/net8.0/RodoLift.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RodoLift")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df1abba77692573ac49089eaf88a8ddb71654bb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b94ef7b522f144b1abdcbf221bd2aeac5b78b06")]
 [assembly: System.Reflection.AssemblyProductAttribute("RodoLift")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RodoLift")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
